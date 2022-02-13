ba253b64a2b5523a23c3629c49d893fec1428558
heycn <x1chennan@qq.com>
2022-02-13 15:58:38 +0800

    创建 button group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import Button from './button'
+import Button from './button.vue'
 import Icon from './icon.vue'
+import ButtonGroup from './button-group.vue'
 
 Vue.component('f-button', Button)
 Vue.component('f-icon', Icon)
+Vue.component('f-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="f-button-group">
+    <slot />
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss">
+  .f-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+    > .f-button {
+      border-radius: 0;
+      margin-left: -1px;
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
