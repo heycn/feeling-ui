1fa06a00539793d152f50046f30e0722678eba3d
heycn <x1chennan@qq.com>
2022-02-13 13:45:19 +0800

    填写 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,29 +1,63 @@
 <template>
-  <button class="f-button">按钮</button>
+  <button class="f-button" :class="{ [`icon-${iconPosition}`]: true }">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot />
+    </div>
+  </button>
 </template>
+
 <script>
-export default {}
+  export default {
+    props: ['icon', 'iconPosition']
+  }
 </script>
+
 <style lang="scss">
-.f-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
-  padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
+  .f-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle; /* 解决 align-items: center; 带来的对不齐 */
 
-  &:hover {
-    border-color: var(--border-color-hover);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+
+    /* 默认 icon 在左边 */
+    > .icon {
+      order: 1;
+      margin-right: 0.3em;
+      margin-left: 0;
+    }
+    > .content {
+      order: 2;
+    }
+
+    /* 设置 icon-position 为 right 时，icon 在右边*/
+    &.icon-right {
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: 0.3em;
+      }
+      > .content {
+        order: 1;
+      }
+    }
   }
-
-  &:active {
-    background-color: var(--button-active-bg);
-  }
-
-  &:focus {
-    outline: none;
-  }
-}
-
 </style>
