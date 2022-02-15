8c0ca219c16c998399d4daa94a445513aefc8ec3
heycn <x1chennan@qq.com>
2022-02-15 19:12:03 +0800

    初步实现响应式

--- src/col.vue
+++ src/col.vue
@@ -13,6 +13,18 @@
       },
       offset: {
         type: [Number, String]
+      },
+      phone: {
+        type: Object,
+        validator(value) {
+          let keys = Object.keys(value)
+          let valid = true
+          keys.forEach(key => {
+            if (!['span', 'offset'].includes(key)) {
+              valid = false
+            }
+          })
+        }
       }
     },
     data() {
@@ -22,8 +34,12 @@
     },
     computed: {
       colClass() {
-        let { span, offset } = this
-        return [span && `col-${span}`, offset && `offset-${offset}`]
+        let { span, offset, phone } = this
+        let phoneClass = []
+        if (phone) {
+          phoneClass = [`col-phone-${phone.span}`]
+        }
+        return [span && `col-${span}`, offset && `offset-${offset}`, ...phoneClass]
       },
       colStyle() {
         return {
@@ -47,6 +63,21 @@
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: ($n / 24) * 100%;
+      }
+    }
+
+    @media (max-width: 576px) {
+      $class-prefix: col-phone-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-phone-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
       }
     }
   }
