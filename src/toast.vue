984d1ad869afe8fc7c5c3cc6a50827c3c82b1842
heycn <x1chennan@qq.com>
2022-02-18 16:27:25 +0800

    完成 toast 测试用例

--- src/toast.vue
+++ src/toast.vue
@@ -19,12 +19,11 @@
     name: 'FeelToast',
     props: {
       autoClose: {
-        type: Boolean,
-        default: true
-      },
-      autoCloseDelay: {
-        type: Number,
-        default: 5
+        type: [Boolean, Number],
+        default: 5,
+        validator(value) {
+          return value === false || typeof value === 'number'
+        }
       },
       closeButton: {
         type: Object,
@@ -69,16 +68,13 @@
         if (this.autoClose) {
           setTimeout(() => {
             this.close()
-          }, this.autoCloseDelay * 1000)
+          }, this.autoClose * 1000)
         }
       },
       close() {
         this.$el.remove()
         this.$emit('close')
         this.$destroy()
-      },
-      log() {
-        console.log('测试')
       },
       onClickClose() {
         this.close()
