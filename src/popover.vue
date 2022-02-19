d52a3372f1fcffd9a4d4b0a6d9e76071ad8006bc
heycn <x1chennan@qq.com>
2022-02-19 21:18:21 +0800

    优化上一次提交

--- src/popover.vue
+++ src/popover.vue
@@ -18,11 +18,16 @@
         this.visible = !this.visible
         if (this.visible === true) {
           this.$nextTick(() => {
-            document.addEventListener( 'click', function x() {
-                this.visible = false
-                document.removeEventListener('click', x)
-              }.bind(this))
+            let eventHandler = () => {
+              this.visible = false
+              console.log('document 隐藏 popover')
+              document.removeEventListener('click', eventHandler)
+            }
+
+            document.addEventListener('click', eventHandler)
           })
+        } else {
+          console.log('vm 隐藏 popover')
         }
       }
     }
@@ -34,12 +39,12 @@
     display: inline-block;
     vertical-align: top;
     position: relative;
-  }
-  .content-wrapper {
-    position: absolute;
-    bottom: 100%;
-    left: 0;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    .content-wrapper {
+      position: absolute;
+      bottom: 100%;
+      left: 0;
+      border: 1px solid red;
+      box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    }
   }
 </style>
