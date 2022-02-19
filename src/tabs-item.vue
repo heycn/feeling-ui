a5b79105184800c0c1a2cf33ede0d8b4e2bc04f5
heycn <x1chennan@qq.com>
2022-02-19 16:20:08 +0800

    增大tab item可点击区域

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -52,6 +52,10 @@
   .tabs-item {
     flex-shrink: 0;
     padding: 0 1em;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    height: 100%;
     &.active {
       background: red;
     }
