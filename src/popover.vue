467071d4677c00aa01040ee404615a3fcb6328e3
heycn <x1chennan@qq.com>
2022-02-20 18:40:42 +0800

    解决抖动 bug

--- src/popover.vue
+++ src/popover.vue
@@ -164,10 +164,12 @@
       }
       &::before {
         border-top-color: black;
+        border-bottom: none;
         top: 100%;
       }
       &::after {
         border-top-color: white;
+        border-bottom: none;
         top: calc(100% - 1px);
       }
     }
@@ -178,10 +180,12 @@
         left: 10px;
       }
       &::before {
+        border-top: none;
         border-bottom-color: black;
         bottom: 100%;
       }
       &::after {
+        border-top: none;
         border-bottom-color: white;
         bottom: calc(100% - 1px);
       }
@@ -196,10 +200,12 @@
       }
       &::before {
         border-left-color: black;
+        border-right: none;
         left: 100%;
       }
       &::after {
         border-left-color: white;
+        border-right: none;
         left: calc(100% - 1px);
       }
     }
@@ -212,10 +218,12 @@
       }
       &::before {
         border-right-color: black;
+        border-left: none;
         right: 100%;
       }
       &::after {
         border-right-color: white;
+        border-left: none;
         right: calc(100% - 1px);
       }
     }
