97f1c03d39900b42dd7ccdfd6b784ed4d48b36be
heycn <x1chennan@qq.com>
2022-02-20 15:23:25 +0800

    功能方面基本完场

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popover" @click.stop="xxx">
+  <div class="popover" @click="onClick" ref="popover">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
@@ -16,22 +16,36 @@
       return { visible: false }
     },
     methods: {
-      xxx() {
-        this.visible = !this.visible
-        if (this.visible === true) {
-          this.$nextTick(() => {
-            document.body.appendChild(this.$refs.contentWrapper)
-            let { width, height, top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
-            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-            let eventHandler = () => {
-              this.visible = false
-              document.removeEventListener('click', eventHandler)
-            }
-            document.addEventListener('click', eventHandler)
-          })
-        } else {
-          console.log('vm 隐藏 popover')
+      positionContent() {
+        document.body.appendChild(this.$refs.contentWrapper)
+        let { width, height, top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      },
+      onClickDocument(e) {
+        if (this.$refs.popover && (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))) {
+          return
+        }
+        this.close()
+      },
+      open() {
+        this.visible = true
+        this.$nextTick(() => {
+          this.positionContent()
+          document.addEventListener('click', this.onClickDocument)
+        })
+      },
+      close() {
+        this.visible = false
+        document.removeEventListener('click', this.onClickDocument)
+      },
+      onClick(event) {
+        if (this.$refs.triggerWrapper.contains(event.target)) {
+          if (this.visible === true) {
+            this.close()
+          } else {
+            this.open()
+          }
         }
       }
     }
