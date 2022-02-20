491b4b5839c77071476173db173ba2048d55f881
heycn <x1chennan@qq.com>
2022-02-20 19:18:30 +0800

    添加插槽的 close API

--- src/popover.vue
+++ src/popover.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="popover" ref="popover">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible" :class="{ [`position-${position}`]: true }">
-      <slot name="content"></slot>
+      <slot name="content" :close="close"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block">
       <slot></slot>
@@ -12,6 +12,22 @@
 <script>
   export default {
     name: 'FeelPopover',
+    props: {
+      position: {
+        type: String,
+        default: 'top',
+        validator(value) {
+          return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+        }
+      },
+      trigger: {
+        type: String,
+        default: 'click',
+        validator(value) {
+          return ['click', 'hover'].indexOf(value) >= 0
+        }
+      }
+    },
     data() {
       return {
         visible: false
@@ -46,22 +62,6 @@
           return 'click'
         } else {
           return 'mouseleave'
-        }
-      }
-    },
-    props: {
-      position: {
-        type: String,
-        default: 'top',
-        validator(value) {
-          return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
-        }
-      },
-      trigger: {
-        type: String,
-        default: 'click',
-        validator(value) {
-          return ['click', 'hover'].indexOf(value) >= 0
         }
       }
     },
