ddeeb67c48b3aae93c5849fb46d2c5d8d3a3728f
heycn <x1chennan@qq.com>
2022-02-21 20:17:12 +0800

    遇到一个 bug

--- src/demo.vue
+++ src/demo.vue
@@ -4,8 +4,16 @@
     <p>{{ (selected && selected[1] && selected[1].name) || '空' }}</p>
     <p>{{ (selected && selected[2] && selected[2].name) || '空' }}</p>
     <div style="padding: 20px">
-      <f-cascader :source="source" popover-height="200px" :selected.sync="selected"></f-cascader>
+      <f-cascader
+        :source.sync="source"
+        popover-height="200px"
+        @update:source="onUpdateSource"
+        @update:selected="onUpdateSelected"
+        :selected.sync="selected"
+        :load-data="loadData"
+      ></f-cascader>
     </div>
+    {{ source }}
   </div>
 </template>
 <script>
@@ -14,10 +22,13 @@
   import db from './db'
 
   function ajax(parentId = 0) {
-    return db.filter(item => item.parent_id == parentId)
+    return new Promise((success, fail) => {
+      setTimeout(() => {
+        let result = db.filter(item => item.parent_id == parentId)
+        success(result)
+      }, 300)
+    })
   }
-
-  console.log(ajax())
 
   export default {
     name: 'demo',
@@ -28,8 +39,28 @@
     data() {
       return {
         selected: [],
-        source: ajax()
+        source: []
       }
+    },
+    created() {
+      ajax(0).then(result => {
+        this.source = result
+      })
+    },
+    methods: {
+      loadData({ id }, updateSource) {
+        ajax(id).then(result => {
+          updateSource(result) // 回调:把别人传给我的函数调用一下
+        })
+      },
+      xxx() {
+        ajax(this.selected[0].id).then(result => {
+          let lastLevelSelected = this.source.filter(item => item.id === this.selected[0].id)[0]
+          this.$set(lastLevelSelected, 'children', result)
+        })
+      },
+      onUpdateSource() {},
+      onUpdateSelected() {}
     }
   }
 </script>
