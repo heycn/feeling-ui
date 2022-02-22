8afea663ecb3ed560e2bc9ce78bcb5ff52bd62c8
heycn <x1chennan@qq.com>
2022-02-22 12:53:17 +0800

    支持固定表头

--- src/table.vue
+++ src/table.vue
@@ -1,33 +1,37 @@
 <template>
-  <div class="feel-table-wrapper">
-    <table class="feel-table" :class="{ bordered, compact, striped: striped }">
-      <thead>
-        <tr>
-          <th><input type="checkbox" @change="onChangeAllItems" ref="allChecked" :checked="areAllItemsSelected" /></th>
-          <th v-if="numberVisible">#</th>
-          <th v-for="column in columns" :key="column.field">
-            <div class="feel-table-header">
-              {{ column.text }}
-              <span v-if="column.field in orderBy" class="feel-table-sorter" @click="changeOrderBy(column.field)">
-                <g-icon name="asc" :class="{ active: orderBy[column.field] === 'asc' }" />
-                <g-icon name="desc" :class="{ active: orderBy[column.field] === 'desc' }" />
-              </span>
-            </div>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(item, index) in dataSource" :key="item.id">
-          <td>
-            <input type="checkbox" @change="onChangeItem(item, index, $event)" :checked="inSelectedItems(item)" />
-          </td>
-          <td v-if="numberVisible">{{ index + 1 }}</td>
-          <template v-for="column in columns">
-            <td :key="column.field">{{ item[column.field] }}</td>
-          </template>
-        </tr>
-      </tbody>
-    </table>
+  <div class="feel-table-wrapper" ref="wrapper">
+    <div :style="{ height, overflow: 'auto' }">
+      <table class="feel-table" :class="{ bordered, compact, striped: striped }" ref="table">
+        <thead>
+          <tr>
+            <th>
+              <input type="checkbox" @change="onChangeAllItems" ref="allChecked" :checked="areAllItemsSelected" />
+            </th>
+            <th v-if="numberVisible">#</th>
+            <th v-for="column in columns" :key="column.field">
+              <div class="feel-table-header">
+                {{ column.text }}
+                <span v-if="column.field in orderBy" class="feel-table-sorter" @click="changeOrderBy(column.field)">
+                  <g-icon name="asc" :class="{ active: orderBy[column.field] === 'asc' }" />
+                  <g-icon name="desc" :class="{ active: orderBy[column.field] === 'desc' }" />
+                </span>
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in dataSource" :key="item.id">
+            <td>
+              <input type="checkbox" @change="onChangeItem(item, index, $event)" :checked="inSelectedItems(item)" />
+            </td>
+            <td v-if="numberVisible">{{ index + 1 }}</td>
+            <template v-for="column in columns">
+              <td :key="column.field">{{ item[column.field] }}</td>
+            </template>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     <div v-if="loading" class="feel-table-loading">
       <g-icon name="loading" />
     </div>
@@ -40,6 +44,9 @@
     components: { FIcon },
     name: 'FeelTable',
     props: {
+      height: {
+        type: [Number, String]
+      },
       orderBy: {
         type: Object,
         default: () => ({})
@@ -80,6 +87,20 @@
         default: false
       }
     },
+    mounted() {
+      let table2 = this.$refs.table.cloneNode(true)
+      this.table2 = table2
+      table2.classList.add('feel-table-copy')
+      this.$refs.wrapper.appendChild(table2)
+      this.updateHeadersWidth()
+      this.onWindowResize = () => this.updateHeadersWidth()
+      window.addEventListener('resize', this.onWindowResize)
+    },
+    beforeDestroy() {
+      this.table2.remove()
+      window.removeEventListener('resize', this.onWindowResize)
+    },
+
     computed: {
       areAllItemsSelected() {
         const a = this.dataSource.map(item => item.id).sort()
@@ -108,6 +129,24 @@
       }
     },
     methods: {
+      updateHeadersWidth() {
+        let table2 = this.table2
+        let tableHeader = Array.from(this.$refs.table.children).filter(
+          node => node.tagName.toLowerCase() === 'thead'
+        )[0]
+        let tableHeader2
+        Array.from(table2.children).map(node => {
+          if (node.tagName.toLowerCase() !== 'thead') {
+            node.remove()
+          } else {
+            tableHeader2 = node
+          }
+        })
+        Array.from(tableHeader.children[0].children).map((th, i) => {
+          const { width } = th.getBoundingClientRect()
+          tableHeader2.children[0].children[i].style.width = width + 'px'
+        })
+      },
       changeOrderBy(key) {
         const copy = JSON.parse(JSON.stringify(this.orderBy))
         let oldValue = copy[key]
@@ -208,6 +247,7 @@
     }
     &-wrapper {
       position: relative;
+      overflow: auto;
     }
     &-loading {
       background: rgba(255, 255, 255, 0.8);
@@ -224,6 +264,13 @@
         height: 50px;
         @include spin;
       }
+    }
+    &-copy {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      background: white;
     }
   }
 </style>
