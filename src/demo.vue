4fd4b447832d51da2c0a526b09a6d44900c698aa
heycn <x1chennan@qq.com>
2022-02-22 12:56:54 +0800

    基本完成 table 组件

--- src/demo.vue
+++ src/demo.vue
@@ -10,8 +10,16 @@
         :order-by.sync="orderBy"
         @update:orderBy="x"
         :loading="loading"
-        height="400px"
-      ></f-table>
+        :height="400"
+        expend-field="description"
+        checkable
+      >
+        <template slot-scope="xxx">
+          <button @click="edit(xxx.item)">编辑</button>
+          <button @click="view(xxx.item)">查看</button>
+          <button>删除</button>
+        </template>
+      </f-table>
     </div>
     <div style="margin: 20px">
       <f-table :columns="columns" :data-source="dataSource" bordered compact :striped="false"></f-table>
@@ -33,7 +41,7 @@
         currentPage: 1,
         selected: [],
         columns: [
-          { text: '姓名', field: 'name' }, // 1
+          { text: '姓名', field: 'name', width: 100 }, // 1
           { text: '分数', field: 'score' }
         ],
         orderBy: {
@@ -42,8 +50,8 @@
         },
         loading: false,
         dataSource: [
-          { id: 1, name: '方方', score: 100 },
-          { id: 2, name: '圆圆', score: 99 },
+          { id: 1, name: '方方', score: 100, description: 'xxxx xxxx' },
+          { id: 2, name: '圆圆', score: 99, description: 'yyyy yyyy' },
           { id: 3, name: '张三', score: 100 },
           { id: 4, name: '李四', score: 99 },
           { id: 5, name: '超人', score: 100 },
@@ -66,6 +74,12 @@
       }
     },
     methods: {
+      edit(item) {
+        alert(`开始编辑${item.id}`)
+      },
+      view(item) {
+        alert(`开始查看${item.id}`)
+      },
       x() {
         this.loading = true
         setTimeout(() => {
